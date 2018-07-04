02bb529c2b0dfb597a02898a8cbcdfc0a41e748d
wanghsukun@we2young.com <wanghsukun@we2young.com>
2018-07-04 13:29:53 +0800

    no message

---
+++ INCIMOpenAPI/INCIMOpenAPI.framework/Headers/INCIMOpenAPI.h
@@ -0,0 +1,19 @@
+//
+//  INCIMOpenAPI.h
+//  INCIMOpenAPI
+//
+//  Created by 王旭焜 on 2018/7/3.
+//  Copyright © 2018年 青岛英凯利信息科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for INCIMOpenAPI.
+FOUNDATION_EXPORT double INCIMOpenAPIVersionNumber;
+
+//! Project version string for INCIMOpenAPI.
+FOUNDATION_EXPORT const unsigned char INCIMOpenAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INCIMOpenAPI/PublicHeader.h>
+
+#import <INCIMOpenAPI/INCIMOpenManager.h>
